0a1da57678e03c16a23213904b4352a9e0629f8b
Ned Palacios <npdoesmc@gmail.com>
2020-05-31 12:37:38 +0800

    use md4c-html for now

--- lib/md4c-lib.c
+++ lib/md4c-lib.c
@@ -2,7 +2,7 @@
  * MD4C: Markdown parser for C
  * (http://github.com/mity/md4c)
  *
- * Copyright (c) 2016-2019 Martin Mitas
+ * Copyright (c) 2016-2017 Martin Mitas
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
@@ -23,55 +23,6 @@
  * IN THE SOFTWARE.
  */
 
-module markdown
-
-import strings
-
-fn C.md_html(orig_input charptr, orig_input_size u32, process_output ProcessFn, userdata voidptr, parser_flags u32, renderer_flags u32) int
-fn C.strchr(str charptr, ch int) byte
-
-// workaround
-struct C.MD_BLOCK_H_DETAIL {
-pub:
-	level u32
-}
-
-struct C.MD_SPAN_WIKILINK_DETAIL {
-pub:
-	target C.MD_ATTRIBUTE
-}
-
-struct C.MD_HTML {
-	process_output ProcessFn
-	userdata voidptr
-	flags u32
-mut:
-	image_nesting_level int
-	escape_map []byte = []byte{len: 256}
-}
-
-const (
-	need_html_esc_flag = 0x1
-	need_url_esc_flag  = 0x2
-	md_html_flag_debug = 0x0001
-	md_html_flag_verbatim_entities = 0x0002
-	md_html_flag_skip_utf8_bom = 0x0004
-)
-
-type ProcessFn = fn (t charptr, s u32, d voidptr)
-
-
-fn write_data(sb &strings.Builder, txt string) {
-	sb.write(txt)
-}
-
-pub fn to_html(input string) string {
-	mut wr := strings.new_builder(200)
-
-	output_fn := fn (txt charptr, s u32, d voidptr) {
-		write_data(&strings.Builder(d), tos3(txt))
-	}
-
-	C.md_html(charptr(input.str), input.len, output_fn, &wr, C.MD_DIALECT_GITHUB, 0)
-	return wr.str().trim_space()
-}
+#include "md4c.c"
+#include "md4c-html.c"
+#include "entity.c"
