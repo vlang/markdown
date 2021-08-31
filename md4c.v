23dcd02b108aacb8760c753e2feaa5ac1b8cd42b
Ned Palacios <npdoesmc@gmail.com>
2021-08-31 08:49:52 +0800

    markdown: update copyright headers

--- md4c.v
+++ md4c.v
@@ -1,9 +1,10 @@
 /*
-* MD4C: Markdown parser for C
+ * MD4C: Markdown parser for C
  * (http://github.com/mity/md4c)
  *
  * Copyright (c) 2016-2019 Martin Mitáš
  * Copyright (c) 2020 Ned Palacios (V bindings)
+ * Copyright (c) 2020-2021 The V Programming Language
  *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
