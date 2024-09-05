08cda2cefaddd3568f7a9428729153d4b7408ef3
Delyan Angelov <delian66@gmail.com>
2024-09-05 15:38:28 +0300

    tests: add a test for a simple md->html code block rendering

--- src/renderer_test.v
+++ src/renderer_test.v
@@ -7,3 +7,22 @@ fn test_render() ! {
 	assert out.len != 0
 	assert out == 'Hello World\nhello bold'
 }
+
+const code_block = 'start
+```
+├── static/
+│   ├── css/
+│   │   └── main.css
+│   └── js/
+│       └── main.js
+└── main.v
+```
+end'
+
+fn test_render_code_block_as_html() {
+	mut xx := HtmlRenderer{}
+	out := render(markdown.code_block, mut xx)!
+	dump(out)
+	assert out.starts_with('<p>start</p><pre><code>├── static/')
+	assert out.ends_with('└── main.v\n</code></pre><p>end</p>')
+}
